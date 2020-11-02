80fc16da46f218d86c0afb475ced95bf07e288a1
Kieran Bingham <kieran.bingham@ideasonboard.com>
2020-11-02 14:18:55 +0000

    gmail: Force regeneration of tokens for scope changes
    The tokens are specific to the scope, so any change to the scope requires
    recreating tokens.
    
    Make the token dependant upon the hash of the scope list.
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

--- gmail.py
+++ gmail.py
@@ -59,18 +59,20 @@ class Thread:
 
 
 class Gmail:
-    # If modifying these scopes, delete the file token.pickle.
-    scopes = ['https://www.googleapis.com/auth/gmail.readonly']
+    # Changing the scopes will require re-authentication, with a new token.
+    scopes = ['https://www.googleapis.com/auth/gmail.modify']
     labels_ = {}
 
     def __init__(self):
         creds = None
-        # The file token.pickle stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
+        # The token file stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the
+        # first time. Any change to the scopes will generate a new token file.
+        token_file = 'token.' + os.path.basename(self.scopes[0])
+        if os.path.exists(token_file):
+            with open(token_file, 'rb') as token:
                 creds = pickle.load(token)
+
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
@@ -80,7 +82,7 @@ class Gmail:
                     'credentials.json', self.scopes)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open('token.pickle', 'wb') as token:
+            with open(token_file, 'wb') as token:
                 pickle.dump(creds, token)
 
         self.service = build('gmail', 'v1', credentials=creds)
